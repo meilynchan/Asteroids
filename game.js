6de1c69a6c74c60e9c70c05e9a0c1d1235043d69
meilynchan <56370194+meilynchan@users.noreply.github.com>
2019-11-13 18:01:29 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,41 +1,76 @@
 
 // game state
 let ship = new Ship()
+let lasers = []
+let rocks = [
+  new Rock(),
+  new Rock(),
+  new Rock(),
+  new Rock(),
+  new Rock(),
+]
 let keyPressed = {}
 
-
-
-// check for user input
+// track user input
 window.addEventListener('keydown', event => {
   keyPressed[event.code] = true
 })
-
 window.addEventListener('keyup', event => {
   keyPressed[event.code] = false
 })
 
+window.addEventListener('keypress', event =>{
+  if(event.code === 'Space'){
+  lasers.push(ship.pewpew())
+  }
+})
+
 // game loop
+let ticks = 0
+
 function loop() {
-  // change game stated
-  if(keyPressed['ArrowLeft']){
+
+  ticks += 1
+  
+  if (ticks % 300 === 0){
+    rocks.push(new Rock())
+  }
+
+  // check user input to change game state
+  if (keyPressed['ArrowLeft']) {
     ship.rotateLeft()
-    }
+  }
 
-  if(keyPressed['ArrowRight']){
+  if (keyPressed['ArrowRight']) {
     ship.rotateRight()
-    }
+  }
 
-  if(keyPressed['ArrowUp']){
+  if (keyPressed['ArrowUp']) {
     ship.thrust()
-    }
+  }
 
-
-
+  // state steps through time
   ship.step()
+  lasers.forEach(l => l.step())
+  rocks.forEach(r => r.step())
+
+  //checks if rocks are hit by lasers
+  rocks.forEach(r => {
+    lasers.forEach(l=> {
+      r.checkForHit(l)
+    })
+  })
+
+  //remove hit things
+  rocks = rocks.filter(r => r.hit === false)
+  lasers = lasers.filter(l => l.hit === false)
+
 
   // draw all
   erase()
   ship.draw()
+  lasers.forEach(l => l.draw())
+  rocks.forEach(r => r.draw())
 
   // trigger loop
   setTimeout(() => loop(), 1000 / 60)
@@ -44,8 +79,11 @@ function loop() {
 // wait for images to load
 async function loadGame() {
   await shipSprite.loaded
-  await meteorSprite.loaded
+  await rockSprite.loaded
   loop()
 }
-
 loadGame()
+
+
+
+
--- ship.js
+++ ship.js
@@ -2,7 +2,6 @@
 // define classes to track our state
 class Ship {
   constructor() {
-    this.sprite = shipSprite.image
     // x,y describe the center of the ship
     this.x = canvas.width / 2
     this.y = canvas.height / 2
@@ -20,40 +19,63 @@ class Ship {
     this.angle += 0.1
   }
   thrust() {
-    this.dx += Math.cos(this.angle)  
+    this.dx += Math.cos(this.angle)
     this.dy += Math.sin(this.angle)
   }
+  pewpew() {
+    let laserDX = 10 * Math.cos(this.angle)
+    let laserDY = 10 * Math.sin(this.angle)
+    return new Laser(this.x, this.y, laserDX, laserDY)
+  }
   step() {
+    // apply speed dx,dy to ship position x,y
     this.x += this.dx
     this.y += this.dy
 
-    //slow down
+    // slow down by bringing speed closer to 0
     this.dx *= 0.98
     this.dy *= 0.98
 
-    if (this.x > canvas.width + this.size){
+    // check if out of bound, to wrap to the other side
+    if (this.x > canvas.width + this.size) {
       this.x = 0
     }
-
-    if (this.x < 0 - this.size){
+    if (this.x < 0 - this.size) {
       this.x = canvas.width
     }
-    
-    if(this.y > canvas.height + this.size){
-    this.y = 0
-}
-
-    if (this.y < 0 - this.size){
-    this.y = canvas.height
-}
-
-
+    if (this.y > canvas.height + this.size){
+      this.y = 0
+    }
+    if (this.y < 0 - this.size) {
+      this.y = canvas.height
+    }
   }
   draw() {
     ctx.save()
     ctx.translate(this.x, this.y)
     ctx.rotate(this.angle)
-    ctx.drawImage(this.sprite, -this.size / 2, -this.size / 2, this.size, this.size)
+    ctx.drawImage(
+      shipSprite.image,
+      -this.size / 2,
+      -this.size / 2,
+      this.size,
+      this.size
+    )
     ctx.restore()
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
